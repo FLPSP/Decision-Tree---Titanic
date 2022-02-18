edc2518cd481a2740b519d2f82237576a9c1e72b
Felipe Santos Pimentel <83166769+FLPSP@users.noreply.github.com>
2022-02-17 23:02:34 -0300

    Update Decision Tree - Titanic.R

--- Decision Tree - Titanic.R
+++ Decision Tree - Titanic.R
@@ -7,6 +7,7 @@ pacotes <- c('titanic',    # carrega a base original titanic_treino
              'Rmisc',      # carrega a função sumarySE para a descritiva
              'scales',     # importa paletas de cores
              'caret'       # Funções úteis para machine learning
+             'plotROC'     # Plotar curva ROC
 )
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
@@ -92,3 +93,61 @@ tab
 
 acc <- (tab[1,1] + tab[2,2])/ sum(tab)
 acc
+
+## Separando a base em treinamento e teste
+set.seed(1)
+bool_treino <- stats::runif(dim(titanic)[1])>.25
+
+treino <- titanic[bool_treino,]
+teste  <- titanic[!bool_treino,]
+
+## Avaliando a árvore na base de treino
+p_treino = stats::predict(arvore, treino)
+c_treino = base::factor(ifelse(p_treino[,2]>.5, "Y", "N"))
+p_teste = stats::predict(arvore, teste)
+c_teste = base::factor(ifelse(p_teste[,2]>.5, "Y", "N"))
+
+tab <- table(c_treino, treino$Survived)
+acc <- (tab[1,1]+tab[2,2])/nrow(treino)
+sprintf('Acurácia na base de treino: %s ', percent(acc))
+
+tab <- table(c_teste, teste$Survived)
+acc <- (tab[1,1]+tab[2,2])/nrow(teste)
+sprintf('Acurácia na base de treino: %s ', percent(acc))
+
+## Calculando a área da curva ROC
+aval_treino <- data.frame(obs=treino$Survived, 
+                         pred=c_treino,
+                         Y = p_treino[,2],
+                         N = 1-p_treino[,2]
+                         )
+
+caret::twoClassSummary(aval_treino, lev=levels(aval_treino$obs))
+
+## Utilizando o mesmo dataframe para fazer a curva ROC:
+CurvaROC <- ggplot2::ggplot(aval_treino, aes(d = obs, m = Y, colour='1')) + 
+  plotROC::geom_roc(n.cuts = 0) +
+  scale_color_viridis_d(direction = -1, begin=0, end=.25) +
+  theme(legend.position = "none") +
+  ggtitle("Curva ROC - base de treino")
+  
+CurvaROC
+
+## Avaliando a árvore na base de teste
+aval_teste <- data.frame(obs=teste$Survived, 
+                         pred=c_teste,
+                         Y = p_teste[,2],
+                         N = 1-p_teste[,2]
+)
+
+twoClassSummary(aval_teste, lev=levels(aval_teste$obs))
+
+## Utilizando o mesmo dataframe para fazer a curva ROC:
+CurvaROC <- ggplot(aval_teste, aes(d = obs, m = Y, colour='a')) + 
+  plotROC::geom_roc(n.cuts = 0) +
+  scale_color_viridis_d(direction = -1, begin=0, end=.25) +
+  theme(legend.position = "none") +
+  ggtitle("Curva ROC - base de teste")
+
+CurvaROC
+
